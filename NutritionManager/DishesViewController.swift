2dabd739430431f4562dfcf31c43cc32d6642fbe
Alexander Theißen <alex.theissen@me.com>
2015-07-20 22:02:19 +0200

    First project init

---
+++ NutritionManager/DishesViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  NutritionManager
+//
+//  Created by Alexander Theißen on 20.07.15.
+//
+//
+
+import UIKit
+
+class DishesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
