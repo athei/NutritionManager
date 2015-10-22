7b30ee5342243fb5301da993cbe86480f188b779
Alexander Theißen <alex.theissen@me.com>
2015-10-22 09:30:25 +0200

    Removed checkmark from dishCell

--- NutritionManager/Source/DishCell.swift
+++ NutritionManager/Source/DishCell.swift
@@ -11,23 +11,4 @@ import UIKit
 class DishCell: UICollectionViewCell {
     @IBOutlet weak var dishName: UILabel!
     @IBOutlet weak var dishImage: UIImageView!
-    @IBOutlet weak var checkMark: SSCheckMark!
-    
-    var editing: Bool = false {
-        didSet {
-            checkMark.hidden = !editing
-        }
-    }
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        checkMark.checkMarkStyle = .GrayedOut
-    }
-    
-    
-    override var selected: Bool {
-        didSet {
-            checkMark.checked = selected
-        }
-    }
 }
